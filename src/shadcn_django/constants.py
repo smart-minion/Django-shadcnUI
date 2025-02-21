73efb77de88d5481000acf0a0837a30bff89acb0
Wiktor Piotorwski <piotorwski.wiktor@outlook.com>
2025-02-20 17:03:58 -0800

    add tailwind and css to initialization command

--- src/shadcn_django/constants.py
+++ src/shadcn_django/constants.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
+COMPONENTS_REPO_URL = "gh:SarthakJariwala/shadcn-django"
 DEFAULT_COMPONENTS_DIRECTORY = Path("templates/cotton")
--- src/shadcn_django/initialize.py
+++ src/shadcn_django/initialize.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
+import copier
 import typer
 from rich.panel import Panel
+from rich.status import Status
 
 from .console import console
-from .constants import DEFAULT_COMPONENTS_DIRECTORY
+from .constants import COMPONENTS_REPO_URL, DEFAULT_COMPONENTS_DIRECTORY
 
 app = typer.Typer(no_args_is_help=True, add_completion=False)
 
@@ -13,12 +17,28 @@ def init():
     Initialize a new project
     """
     DEFAULT_COMPONENTS_DIRECTORY.mkdir(parents=True, exist_ok=True)
+    with Status("Adding tailwind config for shadcn components"):
+        copier.run_copy(
+            src_path=COMPONENTS_REPO_URL,
+            dst_path=Path.cwd(),
+            vcs_ref="main",
+            exclude=["*", "!tailwind.config.js", "!input.css"],
+        )
     console.print(
         Panel(
-            ":rocket: Initialized directory structure for shadcn_django components!\n\n"
-            ":heavy_check_mark: Created " + f"'{DEFAULT_COMPONENTS_DIRECTORY}'",
+            "[bold green]"
+            ":rocket: Initialized shadcn_django components!\n\n"
+            ":heavy_check_mark: Created "
+            + f"'{DEFAULT_COMPONENTS_DIRECTORY}'\n"
+            ":heavy_check_mark: Added 'tailwind.config.js' and CSS required for shadcn components\n\n"
+            "[/bold green]"
+            "[bold yellow]"
+            ":bookmark_tabs: Next steps:\n"
+            "[/bold yellow]"
+            ":arrow_right_hook: Ensure you have tailwind v3 and alpine.js installed\n"
+            ":arrow_right_hook: Add <link rel='stylesheet' href='{% static 'css/output.css' %}'> to your base HTML template\n"
+            ":arrow_right_hook: Run 'npx tailwindcss -i input.css -o static/css/output.css --watch'\n",
             title="Initialization Complete",
-            style="bold green",
             border_style="bold green",
         )
     )
