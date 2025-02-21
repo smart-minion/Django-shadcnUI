3448aa44d48a491b005ab60badbec5aaabac3617
Wiktor Piotorwski <piotorwski.wiktor@outlook.com>
2025-02-20 17:48:36 -0800

    add command for adding components

--- src/shadcn_django/add.py
+++ src/shadcn_django/add.py
@@ -1,12 +1,58 @@
+import importlib.resources
+from typing import Annotated
+
+import copier
+import tomllib
 import typer
+from rich.status import Status
+
+from .console import console
+from .constants import COMPONENTS_REPO_URL, DEFAULT_COMPONENTS_DIRECTORY
 
 app = typer.Typer(no_args_is_help=True)
 
 
+def get_component_dependencies(component: str) -> list[str]:
+    """Read components.toml file and fetch dependencies for the given component"""
+
+    with importlib.resources.open_text("shadcn_django", "components.toml") as f:
+        components = tomllib.loads(f.read())
+
+    return components.get("dependencies", {}).get(component, [])
+
+
 @app.command(name="add")
-def add():
+def add(
+    component: Annotated[
+        str, typer.Argument(help="The name of the component to add")
+    ],
+):
     """
-    Add a new component to your project
+    Add a new shadcn_django component to your project
     """
-    typer.echo("Adding a new component...")
-    typer.echo("Component added successfully!")
+
+    component_dependencies = get_component_dependencies(component)
+    components = {component}
+
+    if component_dependencies:
+        for dependency in component_dependencies:
+            components.add(dependency)
+
+    component_excludes = [
+        item
+        for component in components
+        for item in [f"!{component}", f"!{component}.html"]
+    ]
+    excludes = ["*"] + component_excludes
+
+    with Status(f"Adding {component} component"):
+        copier.run_copy(
+            src_path=COMPONENTS_REPO_URL,
+            dst_path=DEFAULT_COMPONENTS_DIRECTORY,
+            vcs_ref="main",
+            exclude=excludes,
+        )
+
+    console.print(
+        f"[bold green]:heavy_check_mark: Added {component} component successfully![/]",
+    )
