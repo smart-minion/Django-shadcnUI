33f7292d933b069a53c43145bd9dcd63792fd946
Wiktor Piotorwski <piotorwski.wiktor@outlook.com>
2025-03-05 09:28:00 -0800

    no need to run migrations

--- docs/docker_startup.sh
+++ docs/docker_startup.sh
@@ -6,8 +6,5 @@ set -o nounset
 
 PORT=${PORT:-8000}
 
-echo "Running migrations..."
-python ./manage.py migrate --noinput
-
 echo "Starting Gunicorn..."
 gunicorn --bind 0.0.0.0:$PORT --workers 2 --threads 8 --timeout 0 docs.wsgi
